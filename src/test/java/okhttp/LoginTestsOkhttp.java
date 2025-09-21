c6bfd77b6021606232256f406f132cd887d8a418
Maria Z <z0559882272@gmail.com>
2025-09-21 19:22:59 +0300

    create login class

--- src/test/java/okhttp/LoginTestsOkhttp.java
+++ src/test/java/okhttp/LoginTestsOkhttp.java
@@ -3,6 +3,7 @@ package okhttp;
 import com.google.gson.Gson;
 import dto.AuthRequestDTO;
 import dto.AuthResponseDTO;
+import dto.ErrorDTO;
 import okhttp3.*;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -30,6 +31,69 @@ public class LoginTestsOkhttp {
         Assert.assertEquals(response.code(),200);
         AuthResponseDTO responseDTO = gson.fromJson(response.body().string(), AuthResponseDTO.class);
         System.out.println(responseDTO.getToken());
+
+    }
+
+    @Test
+    public void loginWrongEmail() throws IOException {
+        AuthRequestDTO auth = AuthRequestDTO.builder()
+                .username("margogmail.com").password("Mmar123456$").build();
+
+        RequestBody body = RequestBody.create(gson.toJson(auth),JSON);
+
+        Request request = new Request.Builder()
+                .url("https://contactapp-telran-backend.herokuapp.com/v1/user/login/usernamepassword")
+                .post(body)
+                .build();
+        Response response =  client.newCall(request).execute();
+        Assert.assertFalse(response.isSuccessful());
+        Assert.assertEquals(response.code(),401);
+        ErrorDTO errorDTO = gson.fromJson(response.body().string(), ErrorDTO.class);
+        Assert.assertEquals(errorDTO.getStatus(),401);
+        Assert.assertEquals(errorDTO.getMessage(),"Login or Password incorrect");
+        Assert.assertEquals(errorDTO.getPath(),"/v1/user/login/usernamepassword");
+
+    }
+
+    @Test
+    public void loginWrongPassword() throws IOException {
+        AuthRequestDTO auth = AuthRequestDTO.builder()
+                .username("margo@gmail.com").password("Mmar123").build();
+
+        RequestBody body = RequestBody.create(gson.toJson(auth),JSON);
+
+        Request request = new Request.Builder()
+                .url("https://contactapp-telran-backend.herokuapp.com/v1/user/login/usernamepassword")
+                .post(body)
+                .build();
+        Response response =  client.newCall(request).execute();
+        Assert.assertFalse(response.isSuccessful());
+        Assert.assertEquals(response.code(),401);
+        ErrorDTO errorDTO = gson.fromJson(response.body().string(), ErrorDTO.class);
+        Assert.assertEquals(errorDTO.getStatus(),401);
+        Assert.assertEquals(errorDTO.getMessage(),"Login or Password incorrect");
+        Assert.assertEquals(errorDTO.getPath(),"/v1/user/login/usernamepassword");
+
+    }
+
+    @Test
+    public void loginUnregisteredUser() throws IOException {
+        AuthRequestDTO auth = AuthRequestDTO.builder()
+                .username("margo6549@gmail.com").password("Mmar123456$").build();
+
+        RequestBody body = RequestBody.create(gson.toJson(auth),JSON);
+
+        Request request = new Request.Builder()
+                .url("https://contactapp-telran-backend.herokuapp.com/v1/user/login/usernamepassword")
+                .post(body)
+                .build();
+        Response response =  client.newCall(request).execute();
+        Assert.assertFalse(response.isSuccessful());
+        Assert.assertEquals(response.code(),401);
+        ErrorDTO errorDTO = gson.fromJson(response.body().string(), ErrorDTO.class);
+        Assert.assertEquals(errorDTO.getStatus(),401);
+        Assert.assertEquals(errorDTO.getMessage(),"Login or Password incorrect");
+        Assert.assertEquals(errorDTO.getPath(),"/v1/user/login/usernamepassword");
 
     }
 }
